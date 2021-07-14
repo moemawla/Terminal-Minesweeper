3193227efad537c23892269c447a45b521032063
moemawla <moemawla@hotmail.com>
2021-07-14 15:27:57 +1000

    refactor code and add comments for possible raised exceptions

--- src/functions.py
+++ src/functions.py
@@ -2,29 +2,62 @@ import random
 
 characters = 'abcdefghijklmnop'
 
-# This function accepts a list of variables, checks if any of these variables is not an integer and throws 
-# a ValueError exception with the specified error message
-def validate_integers(integers, error_message):
+# This function accepts a list of variables, checks if any of these variables is not an integer and returns "False"
+# else returns "True".
+def validate_integers(integers):
     for integer in integers:
         if not isinstance(integer, int):
-            raise ValueError(error_message)
+            return False
+    
+    return True
 
-# This function accepts a board as a parameter and returns an integer representing the number of rows
+# This function accepts a board as a parameter and returns an integer representing the number of rows.
+# Raises a ValueError if "board" is not a "list".
 def get_number_of_rows(board):
+    if not isinstance(board, list):
+        raise ValueError("The provided board must be of type 'list'")
+
     return len(board)
 
-# This function accepts a board as a parameter and returns an integer representing the number of columns
+# This function accepts a board as a parameter and returns an integer representing the number of columns.
+# Raises a ValueError if "board" is not a "list".
 def get_number_of_columns(board):
     if get_number_of_rows(board) == 0:
         return 0
+    
     return len(board[0])
 
-# This function accepts some parameters to create a board with the specified dimensions and number of mines
-def create_board(number_of_rows, number_of_columns, number_of_mines):
-    # raise an exception if any of the numbers provided is not an integer
-    validate_integers([number_of_rows, number_of_columns, number_of_mines], 'All the parameters provided should be integers')
+# This function accepts a board and a row index as parameters and checks if the index is in range of the board.
+# Raises a ValueError if "board" is not a "list".
+# Raises a ValueError if "row_index" is not an integer.
+def is_row_index_in_range(board, row_index):
+    if not validate_integers([row_index]):
+        raise ValueError("The provided row index must be an integer")
 
-    # raise an exception if the number of mines provided exceeds the number of positions in the board
+    if row_index < 0 or (row_index > (get_number_of_rows(board) - 1)):
+        return False
+    
+    return True
+
+# This function accepts a board and a column index as parameters and checks if the index is in range of the board.
+# Raises a ValueError if "board" is not a "list".
+# Raises a ValueError if "row_index" is not an integer.
+def is_column_index_in_range(board, column_index):
+    if not validate_integers([column_index]):
+        raise ValueError("The provided column index must be an integer")
+
+    if column_index < 0 or column_index > (get_number_of_columns(board) - 1):
+        return False
+    
+    return True
+
+# This function accepts some parameters to create a board with the specified dimensions and number of mines.
+# Raises a ValueError if any of the numbers provided is not an integer.
+# Raises a ValueError if the number of mines provided exceeds the number of positions in the board.
+def create_board(number_of_rows, number_of_columns, number_of_mines):
+    if not validate_integers([number_of_rows, number_of_columns, number_of_mines]):
+        raise ValueError('All the parameters provided should be integers')
+
     if number_of_mines > (number_of_rows * number_of_columns):
        raise ValueError('Number of mines cannot be larger than the number of cells in the board')
 
@@ -48,10 +81,11 @@ def create_board(number_of_rows, number_of_columns, number_of_mines):
     
     return board
 
-# This function accepts a board and prints it to the terminal in a certain style with indices
+# This function accepts a board and prints it to the terminal in a certain style with indices.
+# Raises a ValueError if "board" is not a "list".
+# Raises a ValueError if the number of rows in the board is greater than 16 (the number of characters available).
 def print_board(board):
-    # raise an exception if the number of rows in the board is greater than the number of characters available
-    if len(board) > len(characters):
+    if get_number_of_rows(board) > len(characters):
         raise ValueError(f'Number of rows in the board cannot be larger than {len(characters)}')
     
     # generate the header and row-separator outputs
@@ -77,15 +111,15 @@ def print_board(board):
         output_list.append(output)
         output_list.append(row_separator)
 
-    
     # print the output
-    for row in output_list:
-        print(row)
+    for output_row in output_list:
+        print(output_row)
 
-# This function asks the user for a choice, validates it and returns mixed types for response
-# Returns "None" if the user wants to exit
-# Returns a string message in case of an error
-# Returns a tuple if the row and column indices are acceptable
+# This function asks the user for a choice, validates it and returns mixed types for response.
+# Raises a ValueError if "board" is not a "list".
+# Raises a ValueError if the user inputs unacceptable choice.
+# Returns "None" if the user wants to exit.
+# Returns a tuple if the row and column indices are acceptable.
 def get_user_choice(board):
     # ask the user for a cell coordinates
     try:
@@ -97,25 +131,23 @@ def get_user_choice(board):
         return None
 
     # we need to make sure the choice includes acceptable row and column indices
-    error_message = 'Wrong coordinates'
-
     if len(choice) < 2:
-        return error_message
+        raise ValueError('Wrong coordinates')
 
     try:
         row_index = characters.index(choice[0])
     except ValueError:
-        return error_message
+        raise ValueError('Wrong row coordinates')
     
-    if row_index > (get_number_of_rows(board) - 1):
-        return error_message
+    if not is_row_index_in_range(board, row_index):
+        raise ValueError('Wrong row coordinates')
 
     try:
         column_index = int(choice[1:])
     except ValueError:
-        return error_message
+        raise ValueError('Wrong column coordinates')
 
-    if column_index < 0 or column_index > (get_number_of_columns(board) - 1):
-        return error_message
+    if not is_column_index_in_range(board, column_index):
+        raise ValueError('Wrong column coordinates')
 
     return (row_index, column_index)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,21 @@ from functions import *
 
 def play():
     board = create_board(16, 30, 5)
+
+    while True:
+        try:
+            choice = get_user_choice(board)
+
+            # check if user wants to exit the game
+            if choice == None:
+                print('Bye!')
+                return
+        except ValueError as e:
+            print(str(e))
+            continue
+
+        # user input is valid, process it
+
     #print_board(board)
-    print(get_user_choice(board))
 
 play()

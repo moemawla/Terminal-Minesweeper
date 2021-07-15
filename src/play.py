f73aa8232c059aa5b851d5932de1e40bb5b127ce
moemawla <moemawla@hotmail.com>
2021-07-15 12:27:53 +1000

    catch the KeyboardInterrupt exception and exit gracefully

--- src/play.py
+++ src/play.py
@@ -1,27 +1,27 @@
 from functions import *
 
 def play():
-    # get the game difficulty and board size
     try:
+        # get the game difficulty and board size
         difficulty = get_game_difficulty()
         board_size = get_board_size(difficulty)
-    except KeyboardInterrupt:
-        print('Bye!')
-        return
 
-    # create the board
-    board = create_board(board_size[0], board_size[1], get_number_of_mines(difficulty))
+        # create the board
+        board = create_board(board_size[0], board_size[1], get_number_of_mines(difficulty))
 
-    # print game instructions
-    print('''
+        # print game instructions
+        print('''
 Game Instructions:
 1- To choose a cell pass its row and column indices without a space (ex: b5)
 2- To flag or unflag a cell, type "flag" followed by a space and then the cell's location (ex: flag b5)
 3- To leave game type "exit"
-    ''')
-    input("Press any key to start!")
-    clear_terminal()
-    print_board(board)
+        ''')
+        input("Press any key to start!")
+        clear_terminal()
+        print_board(board)
+    except KeyboardInterrupt:
+        print('Bye!')
+        return
 
     while True:
         try:
@@ -49,6 +49,7 @@ Game Instructions:
         if not can_continue:
             # user chose a cell with a mine, game is lost
             # print the board and show all the mines
+            clear_terminal()
             print_board(board, True)
             print('You Lost :(')
             break
